5872010d352c8f8fb9a244c4a81264b5018abbaa
garlapatijagadeesh <garlapati.jagadeeshk@foundryco.com>
2023-08-03 11:54:35 +0530

    jjjjjjj

---
+++ src/pack1/Test1.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package pack1;
+
+/**
+ * @author garlapati.jagadeeshk
+ *
+ */
+public class Test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("first Class");
+
+	}
+
+}
